8b255105d820190aae9293fa29e990797d5c2bdd
Chris Beck <jcb@mie.utoronto.ca>
2022-11-18 17:46:18 -0500

    Removed part of the answer that uses if-statements. Saving it for Week 3.

--- 2023/Week2/5.py
+++ 2023/Week2/5.py
@@ -1,29 +1,32 @@
-# Write a function that takes the role of a store clerk. 
-#
-# Ask a user to enter two floating point numbers: 
-# the cost of an item and the amount of money remitted to pay for the item.
-# Then respond appropriately: calculate the change due to the customer 
-# or ask the customer for more money. 
-#
-# For example:
-#
-# Cost of the item: 3.56
-# Amount tendered: 5.00
-# Change: 1.44
-# or
-# Cost of the item: 3.56
-# Amount tendered: 3.00
-# Still due: 0.56
-#
-# Hint: You will need some Week 4 material to do this one.
+# Write a program that takes an integer input less than 100000 and 
+# prints if the number is a 5-digit, 4-digit number, 3-digit number, 2-digit number, 
+# or a 1-digit number
 
-cost = float(input("Cost of the item: "))
-tendered = float(input("Amount tendered: "))
+import math
 
-if cost > tendered:
-    print("Still due:", cost - tendered)
-else:
-    print("Change:", tendered - cost)
 
-# Bonus question: figure out how to print out the change/amount due to 
-#                 only two decimal points
+def count_digits_log(number):
+    '''
+    (int) -> int
+    Returns the number of digits in number. -1 is returned is the 
+    number is greater than 99999
+    '''
+    return int(math.log(number, 10)) + 1 # Do you understand why this works?
+
+def count_digits_string(number):
+    '''
+    (int) -> int
+    Returns the number of digits in number. -1 is returned is the 
+    number is greater than 99999
+    '''
+    return len(str(number)) # Do you understand why this works?
+
+number = int(input("Please input a number less than 100000: "))
+
+# Doing it with a log
+num_digits = count_digits_log(number)
+print("You number has", num_digits, "digits.")
+
+# Doing it with a string
+num_digits = count_digits_string(number)
+print("You number has", num_digits, "digits.")
